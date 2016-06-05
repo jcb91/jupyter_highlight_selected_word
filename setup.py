bed606bbae88780ddd2fd49f6d3b4cc978312ef5
Josh Barnes <joshuacookebarnes@gmail.com>
2016-06-06 00:15:05 +0100

    add license and long_description to setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,16 @@ setup(
     author='Joshua Cooke Barnes',
     author_email='joshuacookebarnes@gmail.com',
     url='https://github.com/jcb91/jupyter_highlight_selected_word.git',
+    license='BSD',
+    long_description="""
+        Jupyter notebook extension that enables the CodeMirror addon "Match
+        Highlighter" (see https://codemirror.net/demo/matchhighlighter.html),
+        which highlights all instances of the selected word in the current
+        editor.
+
+        There are a few configurable options, all of which sit under the config
+        key ``highlight_selected_word`` in the ``notebook`` config section.
+    """,
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
